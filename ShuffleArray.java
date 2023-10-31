2365562343e192b7d6ffea5a15efa213394d966b
Shruti Kulkarni <145823175+shruti2222K@users.noreply.github.com>
2023-10-31 19:38:59 +0530

    Update ShuffleArray.java
    without using method collection nd shuffle

--- ShuffleArray.java
+++ ShuffleArray.java
@@ -1,2 +1,25 @@
+import java.util.Random;
 
+public class ShuffleArray {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+        shuffleArray(array);
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void shuffleArray(int[] array) {
+        Random random = new Random();
+
+        for (int i = array.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            
+            // Swapping elements at indexes i and j
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
 	
